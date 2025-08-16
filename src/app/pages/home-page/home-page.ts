7c0f6ee091815f2bb73085f3d1a2dd84b1417a18
swetha4444 <swethasaseendran4@gmail.com>
2025-08-16 17:46:28 -0400

    feat: add HomePage component with template and styles, update PagesModule to include HomePage

---
+++ src/app/pages/home-page/home-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  standalone:  false,
+  imports: [],
+  templateUrl: './home-page.html',
+  styleUrl: './home-page.css'
+})
+export class HomePage {
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginPage } from './login-page/login-page';
 import { CompModule } from '../comp/modules';
+import { HomePage } from './home-page/home-page';
 
 @NgModule({
-  declarations: [LoginPage],
+  declarations: [LoginPage, HomePage],
   imports: [CommonModule, CompModule],
-  exports: [LoginPage]
+  exports: [LoginPage, HomePage]
 })
 export class PagesModule {}
