e0cae80f69d1a98fede5e4c906e253561f83d159
swetha4444 <swethasaseendran4@gmail.com>
2025-08-16 19:50:12 -0400

    refactor: remove Header component from CompModule declarations and exports

--- src/app/comp/modules.ts
+++ src/app/comp/modules.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthComp } from './auth-comp/auth-comp';
 import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
-import { Header } from './header/header';
+
 
 
 @NgModule({
-  declarations: [AuthComp, Header],
+  declarations: [AuthComp],
   imports: [CommonModule, AmplifyAuthenticatorModule],
-  exports: [AuthComp, Header]
+  exports: [AuthComp]
 })
 export class CompModule {}
