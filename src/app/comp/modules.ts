c460e0c10fbfba8e78268cafd8bedb20d10a14fc
swetha4444 <swethasaseendran4@gmail.com>
2025-08-16 18:59:00 -0400

    feat: add Header component with template, styles, and unit tests; update CompModule to include Header

--- src/app/comp/modules.ts
+++ src/app/comp/modules.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthComp } from './auth-comp/auth-comp';
 import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
+import { Header } from './header/header';
 
 
 @NgModule({
-  declarations: [AuthComp],
+  declarations: [AuthComp, Header],
   imports: [CommonModule, AmplifyAuthenticatorModule],
-  exports: [AuthComp]
+  exports: [AuthComp, Header]
 })
 export class CompModule {}
