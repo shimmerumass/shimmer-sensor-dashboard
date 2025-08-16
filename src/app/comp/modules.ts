6ca318a378d83651668b352253310163cc05643f
swetha4444 <swethasaseendran4@gmail.com>
2025-08-16 16:31:15 -0400

    feat: integrate Amplify Authenticator in AuthComp component

--- src/app/comp/modules.ts
+++ src/app/comp/modules.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthComp } from './auth-comp/auth-comp';
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 
 
 @NgModule({
   declarations: [AuthComp],
-  imports: [CommonModule],
+  imports: [CommonModule, AmplifyAuthenticatorModule],
   exports: [AuthComp]
 })
 export class CompModule {}
