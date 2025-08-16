de72042931b46606b8d83daceba60b3d0052858c
swetha4444 <swethasaseendran4@gmail.com>
2025-08-16 18:42:25 -0400

    feat: implement authentication check and navigation in AuthComp, remove unused logout button

--- src/app/comp/auth-comp/auth-comp.ts
+++ src/app/comp/auth-comp/auth-comp.ts
@@ -1,4 +1,6 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, OnDestroy, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticatorService } from '@aws-amplify/ui-angular';
 
 @Component({
   selector: 'app-auth-comp',
@@ -6,11 +8,28 @@ import { Component, Output, EventEmitter } from '@angular/core';
   templateUrl: './auth-comp.html',
   styleUrls: ['./auth-comp.css']
 })
-export class AuthComp {
+export class AuthComp implements OnDestroy {
   @Output() authSuccess = new EventEmitter<void>();
+  private authCheckInterval: any;
+
+  constructor(public authenticator: AuthenticatorService, private router: Router) {
+    this.authCheckInterval = setInterval(() => {
+      if (this.authenticator.authStatus === 'authenticated') {
+        this.router.navigate(['/home']);
+        // window.location.reload();
+        clearInterval(this.authCheckInterval);
+      }
+    }, 300);
+  }
 
   // Call this method after successful login/signup
   onAuthComplete() {
     this.authSuccess.emit();
+  }
+
+  ngOnDestroy() {
+    if (this.authCheckInterval) {
+      clearInterval(this.authCheckInterval);
+    }
   }
 }
