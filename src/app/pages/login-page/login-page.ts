1f962b1e9fedad9fcd58f2b417c674968b5805fb
swetha4444 <swethasaseendran4@gmail.com>
2025-08-16 17:29:59 -0400

    feat: add logout functionality to login page

--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { signOut } from '@aws-amplify/auth';
 
 @Component({
   selector: 'app-login-page',
   standalone: false,
   templateUrl: './login-page.html',
-  styleUrl: './login-page.css'
+  styleUrls: ['./login-page.css']
 })
 export class LoginPage {
-
+  async logout() {
+    await signOut();
+  }
 }
