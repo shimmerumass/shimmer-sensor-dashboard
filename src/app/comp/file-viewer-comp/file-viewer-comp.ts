3fcb20e8dacde4eafc7429466144d7c1e469ba98
swetha4444 <swethasaseendran4@gmail.com>
2025-08-17 00:03:41 -0400

    feat: integrate AG Grid into FileViewerComp and update styles

--- src/app/comp/file-viewer-comp/file-viewer-comp.ts
+++ src/app/comp/file-viewer-comp/file-viewer-comp.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { ColDef } from 'ag-grid-community';
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+
 
 @Component({
   selector: 'app-file-viewer-comp',
   standalone: false,
   templateUrl: './file-viewer-comp.html',
-  styleUrl: './file-viewer-comp.css'
+  styleUrls: ['./file-viewer-comp.css']
 })
 export class FileViewerComp {
+  rowData = [
+    { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
+    { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
+    { make: 'Toyota', model: 'Corolla', price: 29600, electric: false },
+  ];
 
+  colDefs: ColDef[] = [
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' },
+    { field: 'electric' },
+  ];
 }
